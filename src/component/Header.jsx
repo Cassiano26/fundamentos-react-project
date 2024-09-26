8ca4709cf7e85b3131357bb7e191c524a6a2df96
Cassiano26 <candidocassiano2@gmail.com>
2024-09-26 11:01:59 -0300

    Criando componente header com logotipo.

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,8 +1,11 @@
 import styles from './Header.module.css'
+import IgniteLogo from '../assets/Ignite-logo.svg'
+console.log(IgniteLogo);
 
 export function Header() {
     return(
         <header className={styles.header}>
+            <img src={IgniteLogo} alt="Logo tipo do Ignite" />
             <strong>
                 Ignite feed
             </strong>
